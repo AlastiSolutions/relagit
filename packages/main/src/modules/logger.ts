ac29cf9f9800f7b31ffe79f2a417e39b66252c13
Kourosh Alasti <coding@kouroshalasti.com>
2025-10-25 22:19:33 -0700

    fix: swap colors for warn & error

--- packages/main/src/modules/logger.ts
+++ packages/main/src/modules/logger.ts
@@ -8,16 +8,15 @@ export const log = (message: string) => {
 	);
 };
 
-export const error = (message: string) => {
-	console.error(
-		`%${thena.color('[RelaGit]', 'yellow')}%c ${thena.color(message, thena.ASCII.reset)}`,
+export const warn = (message: string) => {
+	console.warn(
 		'color: #e5c062;',
 		'color: #fff;'
 	);
 };
 
-export const warn = (message: string) => {
-	console.warn(
+export const error = (message: string) => {
+	console.error(
 		`%c${thena.color('[RelaGit]', 'red')}%c ${thena.color(message, thena.ASCII.reset)}`,
 		'color: #e56269;',
 		'color: #fff;'
